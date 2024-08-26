21c39507b1ffd2d695df7494fb532a3672858dec
Utkarsh Verma <42279582+utkarshverma455@users.noreply.github.com>
2024-08-27 01:04:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+let capchecked = false;
 function beforeSubmit() {
-        let inputdate = document.querySelector(".inputdate");
-        let outputdate = document.querySelector(".outputdate");
-        console.log('>>>> ' + inputdate.value);
+        if (capchecked) {
+                let inputdate = document.querySelector(".inputdate");
+                let outputdate = document.querySelector(".outputdate");
+                console.log('>>>> ' + inputdate.value);
 
-        let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-        outputdate.value = formattedDate;
+                let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+                outputdate.value = formattedDate;
+        }
+        else
+                alert("Pls check the recapthcha");
+        event.preventDefault();
+
+}
+function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") { var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value); elems["ts"] = JSON.stringify(new Date().getTime()); document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500);
+function capsuccess() {
+        capchecked = true;
 }
